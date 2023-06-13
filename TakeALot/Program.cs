a5649e2bd7f31e41dee1d5b27da913488915e236
Shelton Jacobs <shelton.jacobs@sanlam.co.za>
2023-06-13 17:33:34 +0200

    first commit

---
+++ TakeALot/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using NUnit.Framework;
+
+
+new DriverManager().SetUpDriver(new EdgeConfig());
+
+IWebDriver driver = new EdgeDriver();
+
+driver.Navigate().GoToUrl("https://github.com");
+driver.Manage().Window.Maximize();
+
+
+IWebElement searchInput = driver.FindElement(By.CssSelector("[name='q']"));
+searchInput.SendKeys("selenium");
+searchInput.SendKeys(Keys.Enter);
+
+
+driver.Quit();
+IList<string> repositories = driver.FindElements(By.CssSelector(".repo-list-item"))
+    .Select(item => item.Text.ToLower())
+    .ToList();
+
+IList<string> containingSearch = repositories.Where(item => item.Contains("selenium"))
+    .ToList();
+
+Assert.AreEqual(containingSearch, repositories);
+
+
+
